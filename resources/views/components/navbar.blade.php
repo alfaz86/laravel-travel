5d57af656a9310c5734abf7d479e7b5f8209786f
alfaz86 <hidecode403@gmail.com>
2024-12-05 11:48:37 +0700

    update auth navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -23,12 +23,12 @@
 
                     <a href="{{ route('booking.list.page') }}" class="inline-flex items-center px-1 pt-1 border-b-2 
                               {{ Route::is('booking.list.page') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500' }} 
-                              text-sm font-medium hover:border-gray-300 hover:text-gray-700" data-role="user">
+                              text-sm font-medium hover:border-gray-300 hover:text-gray-700" style="display: none;" data-role="user">
                         Daftar Booking
                     </a>
                     <a href="{{ route('qr.reader.page') }}" class="inline-flex items-center px-1 pt-1 border-b-2 
                               {{ Route::is('qr.reader.page') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500' }} 
-                              text-sm font-medium hover:border-gray-300 hover:text-gray-700" data-role="admin">
+                              text-sm font-medium hover:border-gray-300 hover:text-gray-700" style="display: none;" data-role="admin">
                         Scan Tiket
                     </a>
                 </div>
@@ -50,7 +50,7 @@
 
             <!-- Right side: Login button -->
             <div class="hidden sm:flex border-t border-gray-200 pt-4 pb-3">
-                <a href="{{ route('auth.login') }}" class="block pl-3 pr-4 py-2 text-base font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-800" id="loginBtn">Login</a>
+                <a href="{{ route('auth.login') }}" class="block pl-3 pr-4 py-2 text-base font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-800" style="display: none;" id="loginBtn">Login</a>
             </div>
 
             <div class="hidden sm:flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse" id="profile" style="display: none;">
@@ -97,11 +97,11 @@
             </a>
 
             <a href="{{ route('booking.list.page') }}" class="block pl-3 pr-4 py-2 border-l-4 
-                      {{ Route::is('booking.list.page') ? 'border-indigo-500 text-indigo-700 bg-indigo-50' : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800' }}" data-role="user">
+                      {{ Route::is('booking.list.page') ? 'border-indigo-500 text-indigo-700 bg-indigo-50' : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800' }}" style="display: none;" data-role="user">
                 Daftar Booking
             </a>
             <a href="{{ route('qr.reader.page') }}" class="block pl-3 pr-4 py-2 border-l-4 
-                      {{ Route::is('qr.reader.page') ? 'border-indigo-500 text-indigo-700 bg-indigo-50' : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800' }}" data-role="admin">
+                      {{ Route::is('qr.reader.page') ? 'border-indigo-500 text-indigo-700 bg-indigo-50' : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800' }}" style="display: none;" data-role="admin">
                 Scan Tiket
             </a>
         </div>
@@ -122,66 +122,66 @@
 <script>
     $(function() {
         const authUser = getAuthUser();
-        const authUserProfile = document.querySelectorAll('.user-name, .user-email');
         
-        // DOM elements
-        const mobileMenuButton = document.querySelector('[aria-controls="mobile-menu"]');
-        const mobileMenu = document.getElementById('mobile-menu');
-        const mobileMenuLogin = document.getElementById('mobile-menu-login');
-        const mobileMenuLogout = document.getElementById('mobile-menu-logout');
-        const loginButton = document.getElementById('loginBtn');
-        const userProfileMenu = document.querySelector('#profile');
-        const logoutButton = document.querySelector('.logoutBtn');
-        const navUser = $('a[data-role="user"]');
-        const navAdmin = $('a[data-role="admin"]');
+        // DOM Elements
+        const $mobileMenuButton = $('[aria-controls="mobile-menu"]');
+        const $mobileMenu = $('#mobile-menu');
+        const $mobileMenuLogin = $('#mobile-menu-login');
+        const $mobileMenuLogout = $('#mobile-menu-logout');
+        const $loginButton = $('#loginBtn');
+        const $userProfileMenu = $('#profile');
+        const $logoutButton = $('.logoutBtn');
+        const $navUser = $('a[data-role="user"]');
+        const $navAdmin = $('a[data-role="admin"]');
+        const $authUserProfile = $('.user-name, .user-email');
 
         // Toggle mobile menu visibility
         const toggleMobileMenu = () => {
-            const isExpanded = mobileMenuButton.getAttribute('aria-expanded') === 'true';
-            mobileMenuButton.setAttribute('aria-expanded', !isExpanded);
-            mobileMenu.classList.toggle('hidden');
+            const isExpanded = $mobileMenuButton.attr('aria-expanded') === 'true';
+            $mobileMenuButton.attr('aria-expanded', !isExpanded);
+            $mobileMenu.toggleClass('hidden');
         };
 
         // Update user profile information
         const updateUserProfile = () => {
-            authUserProfile.forEach(el => {
-                if (el.classList.contains('user-name')) {
-                    el.textContent = authUser.name;
+            $authUserProfile.each(function() {
+                if ($(this).hasClass('user-name')) {
+                    $(this).text(authUser.name);
                 } else {
-                    el.textContent = authUser.email;
+                    $(this).text(authUser.email);
                 }
             });
         };
 
         // Handle UI updates based on authentication status
         const updateUIForAuthUser = () => {
-            loginButton.style.display = 'none';
-            userProfileMenu.style.display = '';
-            mobileMenuLogin.style.display = 'none';
-            mobileMenuLogout.style.display = 'block';
+            $userProfileMenu.css('display', '')
+            $loginButton.hide();
+            $mobileMenuLogin.hide();
+            $mobileMenuLogout.show();
             updateUserProfile();
-            
+
             if (authUser.role === 'user') {
-                navUser.show();
-                navAdmin.hide();
+                $navUser.show();
+                $navAdmin.hide();
+            } else {
+                $navUser.show();
+                $navAdmin.show();
             }
         };
 
         // Handle UI updates for non-authenticated user
         const updateUIForGuest = () => {
-            loginButton.style.display = 'block';
-            userProfileMenu.style.display = 'none';
-            mobileMenuLogin.style.display = 'block';
-            mobileMenuLogout.style.display = 'none';
-
-            navAdmin.hide();
-            navUser.hide();
+            $loginButton.show();
+            $userProfileMenu.hide();
+            $mobileMenuLogin.show();
+            $mobileMenuLogout.hide();
         };
 
         // Handle UI for auth page
         const updateUIForAuthPage = () => {
-            loginButton.style.display = 'none';
-            userProfileMenu.style.display = 'none';
+            $loginButton.hide();
+            $userProfileMenu.hide();
         };
 
         // Initialize UI based on user authentication status
@@ -194,7 +194,7 @@
         }
 
         // Add event listener for mobile menu toggle
-        mobileMenuButton.addEventListener('click', toggleMobileMenu);
+        $mobileMenuButton.on('click', toggleMobileMenu);
     });
 
     async function logout() {
@@ -204,5 +204,4 @@
         }
     }
 </script>
-
 @endpush
